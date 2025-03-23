c33df0ab55e102aa883b9dbd627cfda23ebd6ca6
Toufiqur Rahman <toufiquer1998@gmail.com>
2025-03-24 01:48:33 +0600

    service section almost done

---
+++ script/service.js
@@ -0,0 +1,6 @@
+document.getElementById("about-button").addEventListener("click", function() {
+    window.location.href = "About.html";
+});
+document.getElementById("product-button").addEventListener("click", function() {
+    window.location.href = "product.html";
+});
