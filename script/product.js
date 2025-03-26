765c1031270d158ba6e3621cc9062d77517a909f
Toufiqur Rahman <toufiquer1998@gmail.com>
2025-03-26 14:26:54 +0600

    product section started

---
+++ script/product.js
@@ -0,0 +1,6 @@
+document.getElementById("about-button").addEventListener("click", function() {
+    window.location.href = "About.html";
+});
+document.getElementById("service-button").addEventListener("click", function() {
+    window.location.href = "service.html";
+});
