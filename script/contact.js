e311ad1b72894c1a648bb48a8dccc636722c9276
Toufiqur Rahman <toufiquer1998@gmail.com>
2025-03-30 01:41:26 +0600

    almost done

---
+++ script/contact.js
@@ -0,0 +1,9 @@
+document.getElementById("service-button").addEventListener("click", function() {
+    window.location.href = "service.html";
+});
+document.getElementById("product-button").addEventListener("click", function() {
+    window.location.href = "product.html";
+});
+document.getElementById("about-button").addEventListener("click", function() {
+    window.location.href = "About.html";
+});
